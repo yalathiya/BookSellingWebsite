eda33cb8367259dd51f42c6d0bb57fd2522e5c70
yalathiya <yashlathiya59ppsv@gmail.com>
2023-02-23 23:48:39 +0530

    Counter class is not implemented yet.

--- book-selling-site/src/App.js
+++ book-selling-site/src/App.js
@@ -1,9 +1,12 @@
 import Home from "./pages/Home";
+import Counter from "./component/Counter.js";
+import { render } from "@testing-library/react";
 
 function App() {
   return (
     <>
       <Home />
+      <Counter />
     </>
   );
 }
---
+++ book-selling-site/src/component/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Component } from "react";
+
+class Counter extends Component {
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       count: { useState }(0),
+  //     };
+  //   }
+  //   increaseCounter = () => {
+  //     count.setState = this.state.count + 1;
+  //   };
+  //   render() {
+  //     return (
+  //       <>
+  //         <div className="counterView">Counter : {this.state.count}</div>
+  //         <button onClick="increaseCounter()">Increase Counter</button>
+  //       </>
+  //     );
+  //   }
+}
+
+export default Counter;
