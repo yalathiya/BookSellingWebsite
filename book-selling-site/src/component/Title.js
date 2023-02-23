dbac10bc9d746c595fcda896d0ec10eb0a723cfc
yalathiya <yashlathiya59ppsv@gmail.com>
2023-02-23 22:03:24 +0530

    Used Title in Home

---
+++ book-selling-site/src/component/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <>
+      <div className="title">This is Title</div>
+    </>
+  );
+};
+
+export default Title;
---
+++ book-selling-site/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../component/Title.js";
+
+const Home = () => {
+  return (
+    <>
+      <Title />
+    </>
+  );
+};
+
+export default Home;
