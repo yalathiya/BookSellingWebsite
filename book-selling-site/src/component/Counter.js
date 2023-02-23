d0c343d2ac54cf52c68d1353549763ed12ff2903
yalathiya <yashlathiya59ppsv@gmail.com>
2023-02-24 00:08:51 +0530

    Counter class is implemented

--- book-selling-site/src/component/Counter.js
+++ book-selling-site/src/component/Counter.js
@@ -2,23 +2,26 @@ import React, { useState } from "react";
 import { Component } from "react";
 
 class Counter extends Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       count: { useState }(0),
-  //     };
-  //   }
-  //   increaseCounter = () => {
-  //     count.setState = this.state.count + 1;
-  //   };
-  //   render() {
-  //     return (
-  //       <>
-  //         <div className="counterView">Counter : {this.state.count}</div>
-  //         <button onClick="increaseCounter()">Increase Counter</button>
-  //       </>
-  //     );
-  //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    return (
+      <>
+        <div className="counter">Counter : {this.state.count}</div>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increment Counter
+        </button>
+      </>
+    );
+  }
 }
 
 export default Counter;
